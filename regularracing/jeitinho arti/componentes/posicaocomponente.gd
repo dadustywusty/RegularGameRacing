9cad0c32385fd7228bd85b48616579294d571285
thatartemi <thatartemi@gmail.com>
2026-04-13 21:07:16 -0400

    quase hein

--- regularracing/jeitinho arti/componentes/posicaocomponente.gd
+++ regularracing/jeitinho arti/componentes/posicaocomponente.gd
@@ -5,12 +5,31 @@ var posicao : int
 var volta := 1
 var ultimo_cp_idx := -1
 var progresso := 0.0
+var cp_passados := 0
+
+@onready var checkpoints = get_tree().get_first_node_in_group("container checkpoints").get_children()
+@onready var total_checkpoints = checkpoints.size()
 
 func tick() -> void:
-	var checkpoints = get_parent().get_parent().get_node("checkpoints").get_children()
+	if checkpoints.is_empty():
+		return
+	
 	var base = (volta * 1000) + (ultimo_cp_idx * 10)
 	var prox_idx = (ultimo_cp_idx + 1) % checkpoints.size()
 	var prox_cp = checkpoints[prox_idx]
 	var distancia = get_parent().global_position.distance_to(prox_cp.global_position)
 	
 	progresso = base - distancia
+
+func passou_checkpoint(checkpoint_id: int) -> void:
+	if checkpoint_id > ultimo_cp_idx or (ultimo_cp_idx == total_checkpoints - 1 and checkpoint_id == 0):
+		if checkpoint_id == 0 and ultimo_cp_idx != -1:
+			if cp_passados  >= total_checkpoints * 0.8:
+				volta += 1
+				cp_passados = 0
+		if checkpoint_id != ultimo_cp_idx:
+			ultimo_cp_idx = checkpoint_id
+			cp_passados += 1
+
+func _on_respawn_componente_checkpoint_passado(id: int) -> void:
+	passou_checkpoint(id)
--- regularracing/jeitinho arti/componentes/respawncomponente.gd
+++ regularracing/jeitinho arti/componentes/respawncomponente.gd
@@ -1,6 +1,8 @@
 extends Area3D
 class_name RespawnComponente
 
+signal checkpoint_passado(id: int)
+
 @export var corpo : CharacterBody3D
 var checkpoint_atual : Area3D
 var respawn : Node3D
@@ -8,6 +10,7 @@ var respawn : Node3D
 func _on_area_entered(area: Area3D) -> void:
 	if area.is_in_group("checkpoint"):
 		checkpoint_atual = area
+		checkpoint_passado.emit(area.get_index())
 		for node in checkpoint_atual.get_children():
 			if node.is_in_group("ponto respawn"):
 				respawn = node
